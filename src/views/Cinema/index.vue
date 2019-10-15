52fdacf76b9083fb22dd336ce120f386860e18df
黄烈剑 <QQwwee123>
2019-10-15 20:09:17 +0800

    ###提交

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="main">
+        <Header title="喵喵影院"/>
+        <TabBar/>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import Header from '@/components/Header';
+    import TabBar from '@/components/TabBar';
+    export default {
+        name: "Cinema",
+        components: {
+            Header,
+            TabBar
+        }
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
